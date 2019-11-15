94ca7edf1d5d04c5d663508c853e4833907c484f
Sahil Kakkar <sahilck2009@gmail.com>
2019-11-15 13:55:33 -0500

    first commit

---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Menu() {
+  return (
+    <div className="Menu">
+        <p>
+          This is the Menu Bar
+        </p>
+    </div>
+  );
+}
+
+export default Menu;
